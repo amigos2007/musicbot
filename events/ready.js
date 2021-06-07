de85f771702de84f221acb88f48bad156bc33a5d
amigos2007 <68872510+amigos2007@users.noreply.github.com>
2021-06-07 21:54:59 +0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setActivity(`V12 MÜZİK BOTU BY İHSANDEMİR||${prefix}yardım | ${client.guilds.cache.size} Sunucu ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcı`, { type: "PLAYING"});
+  client.user.setActivity(`NE BAKIYON LAN`, { type: "PLAYING"});
   
   console.log(`${client.user.username}: Şu an ` + client.guilds.cache.size + ` adet sunucuya ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} adet kullanıcıya ve ${client.channels.cache.size} adet kanala hizmet veriliyor!`);
 };
